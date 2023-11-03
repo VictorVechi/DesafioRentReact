3364ba1bedea2180c942eb9cd2a242e30b6b9a99
VictorVechi <victorhvechi@gmail.com>
2023-11-02 21:46:13 -0300

    Cria o componente Field

---
+++ src/components/common/Field/Field.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleField } from './field.styles'
+
+const Field = () => {
+  return (
+    <StyleField>
+        
+    </StyleField>
+  )
+}
+
+export default Field
