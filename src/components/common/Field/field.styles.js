d3c191c56d572b27baf08d31af85c36d2a0dff4a
VictorVechi <victorhvechi@gmail.com>
2023-11-03 00:17:33 -0300

    adiciona estilo ao componente field

--- src/components/common/Field/field.styles.js
+++ src/components/common/Field/field.styles.js
@@ -3,6 +3,30 @@ import styled from "styled-components";
 
 
 export const StyleField = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
 
+    input {
+        width: 60%;
+        height: 25px;
+        border: 1px solid ${(props)=>props.theme.orangeRent};
+        border-radius: 5px;
+        padding: 0 10px;
+        background-color: ${(props)=>props.theme.blackOpacidade};
+        color: ${(props)=>props.theme.white};
+        font-size: 16px;
+        font-weight: 500;
+        outline: none;
+    }  
+
+
+    label {
+        width: 30%;
+        height: 25px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
 `
