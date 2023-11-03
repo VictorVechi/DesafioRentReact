ac918b9f7f77f706fd9d62614dce58bc9ccd3c90
VictorVechi <victorhvechi@gmail.com>
2023-11-03 11:32:30 -0300

    adiciona propriedade checked

--- src/components/common/Field/Field.jsx
+++ src/components/common/Field/Field.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { StyleField } from './field.styles'
 
-const Field = ({nomeInput, textoLabel, tipo, textoInput, classe,valorMaximo, valorMinimo, func}) => {
+const Field = ({value,check, nomeInput, textoLabel, tipo, textoInput, classe,valorMaximo, valorMinimo, func}) => {
   return (
     <StyleField>
         <label htmlFor={nomeInput}>{textoLabel}</label>
-        <input className={classe} min={valorMinimo} max={valorMaximo} name={nomeInput} type={tipo} placeholder={textoInput} required onChange={func}/>
+        <input checked={check}  value={value} className={classe} min={valorMinimo} max={valorMaximo} name={nomeInput} type={tipo} placeholder={textoInput} required onChange={func}/>
     </StyleField>
   )
 }
