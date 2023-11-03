c959040420682886bfb1574660080023612cb383
VictorVechi <victorhvechi@gmail.com>
2023-11-03 00:18:56 -0300

    adiciona componentes e estilo à página de cadastro

--- src/pages/CadastroVeiculo.jsx
+++ src/pages/CadastroVeiculo.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import { StyleCadastroVeiculo } from './cadastroVeiculo'
+import Form from '../components/View/Form/Form.jsx'
+import Header from '../components/View/Header/Header.jsx'
+import { CaretDown } from 'phosphor-react'
+import Lista from '../components/View/Lista/Lista.jsx'
 
 const CadastroVeiculo = () => {
   return (
     <StyleCadastroVeiculo>
-        
+        <Header/>
+        <section className='section-form'>
+          <Form />
+        </section>
+        <section className='section-veiculos'>
+          <a href='#lista-veiculos'><CaretDown size={64} className='seta'/></a>
+          <Lista idSection={'lista-veiculos'}/>
+        </section>
     </StyleCadastroVeiculo>
   )
 }
--- src/pages/cadastroVeiculo.js
+++ src/pages/cadastroVeiculo.js
@@ -3,5 +3,37 @@ import styled from "styled-components";
 
 
 export const StyleCadastroVeiculo = styled.div`
+    height: 100vh;
+.section-form {
+    height: 84%;
+    background-image: url(/imagemBackground.jpg);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.section-veiculos {
+    height: 100%;
+    background-color: ${(props)=>props.theme.black};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.seta {
+    color: ${(props)=>props.theme.orangeRent};
+    
+}
+
+.seta:hover {
+    color: ${(props)=>props.theme.white};
+}
+
     
 `
