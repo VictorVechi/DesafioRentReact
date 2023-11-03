1c3a1da1e02656b452f5bd4758665aad800d69a6
VictorVechi <victorhvechi@gmail.com>
2023-11-03 00:16:49 -0300

    estiliza o componente botão

--- src/components/common/Button/button.styles.js
+++ src/components/common/Button/button.styles.js
@@ -2,7 +2,22 @@ import styled from "styled-components";
 
 
 export const StyleButton = styled.button`
+    width: 30%;
+    height: 35px;
+    border: 2px solid ${(props)=>props.theme.orangeRent};
+    border-radius: 5px;
+    background-color: ${(props)=>props.theme.orangeRent};
+    color: ${(props)=>props.theme.white};
+    font-size: 16px;
+    font-weight: 800;
+    outline: none;
+    cursor: pointer;
+    transition: 0.3s;
 
+    &:hover {
+        background-color: ${(props)=>props.theme.white};
+        color: ${(props)=>props.theme.orangeRent};
+    }
 
 
 `
