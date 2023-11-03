997378c988ef577c59d6e03fdfb8bc5429a0d27d
VictorVechi <victorhvechi@gmail.com>
2023-11-03 00:16:36 -0300

    adiciona cores para tema

---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+
+
+export const theme = {
+    black: "#13100D",
+    blackOpacidade: "#13100DE0",
+    white: "#FFF",
+    orangeRent: "#f6a60c",
+    darkYellowRent: "#F8B32B",
+    yellowRent: "#FEBB02",
+    verderRent: "#198754",
+}
