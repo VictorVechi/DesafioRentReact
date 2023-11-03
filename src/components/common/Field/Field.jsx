e9f1997fa1afccadfcabcef73614dbcd3de43dac
VictorVechi <victorhvechi@gmail.com>
2023-11-03 00:17:25 -0300

    cria a estrutura do componente

--- src/components/common/Field/Field.jsx
+++ src/components/common/Field/Field.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { StyleField } from './field.styles'
 
-const Field = () => {
+const Field = ({nomeInput, textoLabel, tipo, textoInput, classe,valorMaximo, valorMinimo, func}) => {
   return (
     <StyleField>
-        
+        <label htmlFor={nomeInput}>{textoLabel}</label>
+        <input className={classe} min={valorMinimo} max={valorMaximo} name={nomeInput} type={tipo} placeholder={textoInput} required onChange={func}/>
     </StyleField>
   )
 }
