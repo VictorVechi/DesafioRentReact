0090f60004d2ff24cc44659cf750e543cb98e518
VictorVechi <victorhvechi@gmail.com>
2023-11-03 00:18:09 -0300

    aplica estilo ao formulário

--- src/components/View/Form/form.styles.js
+++ src/components/View/Form/form.styles.js
@@ -1,9 +1,24 @@
 import styled from "styled-components";
 
-
-
 export const StyleForm = styled.form`
+    width: 30% ;
+    height: 80%;
+    border: 3px solid ${(props)=>props.theme.orangeRent};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    backdrop-filter: blur(3px);
+    -webkit-backdrop-filter: blur(12px);
+    background-color: ${(props)=>props.theme.blackOpacidade};
+    box-shadow: -4px -4px 8px 0px rgba(0, 0, 0, 0.25), 4px 4px 8px 0px rgba(0, 0, 0, 0.25);
+    
+    h2, h3{
+        color: ${(props)=>props.theme.orangeRent};
+    }
 
-
+    label {
+        color: ${(props)=>props.theme.white};
+    }
 
 `
