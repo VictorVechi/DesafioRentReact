57f2a68dbad419084f1fb3a1bac90d7e3bc4b0a5
VictorVechi <victorhvechi@gmail.com>
2023-11-02 23:16:02 -0300

    adiciona estilo global e temas ao projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import CadastroVeiculo from './pages/CadastroVeiculo'
+import CadastroVeiculo from './pages/CadastroVeiculo.jsx'
+import { GlobalStyle } from './styles/GlobalStyle.styles.js'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CadastroVeiculo/>
+    <GlobalStyle/>
+    <ThemeProvider theme={theme}>
+      <CadastroVeiculo/>
+    </ThemeProvider>
   </React.StrictMode>,
 )
