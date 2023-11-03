955008aa7017a52ad0c03c1ff04537480630ce57
VictorVechi <victorhvechi@gmail.com>
2023-11-03 12:22:53 -0300

    adiciona condição para o elemento retornado

--- src/components/common/Field/Field.jsx
+++ src/components/common/Field/Field.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 import { StyleField } from './field.styles'
 
 const Field = ({value,check, nomeInput, textoLabel, tipo, textoInput, classe,valorMaximo, valorMinimo, func}) => {
+    if(tipo === 'checkbox'){
+        return (
+            <StyleField>
+                <label htmlFor={nomeInput}>{textoLabel}</label>
+                <input checked={check} value={value} className={classe} name={nomeInput} type={tipo} placeholder={textoInput} onChange={func}/>
+                
+            </StyleField>
+          )
+    }
   return (
+    
     <StyleField>
         <label htmlFor={nomeInput}>{textoLabel}</label>
-        <input checked={check}  value={value} className={classe} min={valorMinimo} max={valorMaximo} name={nomeInput} type={tipo} placeholder={textoInput} required onChange={func}/>
+        <input checked={check} value={value} className={classe} min={valorMinimo} max={valorMaximo} name={nomeInput} type={tipo} placeholder={textoInput} required onChange={func}/>
+        
     </StyleField>
   )
 }
